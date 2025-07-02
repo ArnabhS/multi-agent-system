1a3bbcb5f76aef5a6ddec7bd09f90f4555eb19a4
ArnabhS <sinhaarnabh888@gmail.com>
2025-07-02 11:12:23 +0530

    project setup

---
+++ server/src/app.ts
@@ -0,0 +1,41 @@
+import express from "express";
+import dotenv from "dotenv";
+import cors from "cors"
+import { connectDB } from "./config/db.js";
+
+
+dotenv.config();
+const app = express();
+
+app.use(express.json());
+
+app.use(
+  cors({
+  origin: [ process.env.CORS_ORIGIN || "*" ], 
+  credentials: true, 
+  methods: ["GET", "POST", "PUT", "DELETE", "PATCH", "OPTIONS"],
+  allowedHeaders: [
+  "Origin",
+  "Content-Type",
+  "Accept",
+  "Authorization",
+  "X-Request-With",
+  ],
+  })
+  );
+
+connectDB();
+
+const PORT = process.env.PORT || 5000;
+
+const startServer = async () => {
+  try {
+    app.listen(PORT, () => {
+      console.log(`Server running on http://localhost:${PORT}`);
+    });
+  } catch (err) {
+    console.error('Failed to start server:', err);
+  }
+};
+
+startServer();
